8d28400f92ccbe0594ac88861b88a8f730ffe82e
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-08-18 21:43:48 -0400

    FEAT[logic]: ADD feat for editing grid size dinamically

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
-const rows = 16;
+const MAX_SQUARES = 100;
+let rows = 16;
+let columns = 16
 const container = document.querySelector('.container');
 // const square = document.createElement('div');
 
 // size variable
-let size = container.offsetWidth;
+const size = container.offsetWidth;
 // let width = box.offsetWidth;
 // let height = box.offsetHeight;
-for (let i = 0; i < (rows * rows ); i++) {
-    let square = document.createElement('div');
-    square.style.width = `${Math.floor(size / 16)}px`;
-    square.style.height = `${Math.floor(size / 16)}px`;
-    square.textContent = i;
-    container.appendChild(square);
-}
+generateGrid(rows);
 
 // Mouse over event listener
 container.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'blue';
 })
+
+// Prompt for new grid
+const btnGrid = document.querySelector('.btnGrid');
+btnGrid.addEventListener('click', () => {
+    rows = parseInt(prompt('Enter grid size: '));
+    if (rows <= MAX_SQUARES) {
+        clearGrid();
+        generateGrid(rows);
+    }
+})
+
+function clearGrid() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(sqr => {sqr.remove()});
+}
+
+function generateGrid(rows) {
+    if (rows <= MAX_SQUARES) {
+        for (let i = 0; i < (rows * rows ); i++) {
+            let square = document.createElement('div');
+            square.classList.toggle('square');
+            square.style.width = `${Math.floor(size / rows)}px`;
+            square.style.height = `${Math.floor(size / rows)}px`;
+            square.textContent = i;
+            container.appendChild(square);
+        }
+    }
+}
