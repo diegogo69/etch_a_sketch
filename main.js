784f4cc4722b5b1392afe951c0a44bc7f31b5adb
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-08-18 21:00:04 -0400

    FEAT[struct]: ADD basic structure and logic for grid

---
+++ main.js
@@ -0,0 +1,15 @@
+const rows = 16;
+const container = document.querySelector('.container');
+// const square = document.createElement('div');
+
+// size variable
+let size = container.offsetWidth;
+// let width = box.offsetWidth;
+// let height = box.offsetHeight;
+for (let i = 0; i < (rows * rows ); i++) {
+    let square = document.createElement('div');
+    square.style.width = `${Math.floor(size / 16)}px`;
+    square.style.height = `${Math.floor(size / 16)}px`;
+    square.textContent = i;
+    container.appendChild(square);
+}
